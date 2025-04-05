9c957594cb98be52ddcf60193bd1408c6d22ed5d
Ульяна <u682148@gmail.com>
2025-04-05 18:30:56 +0300

    feat: add word_frequency

--- main.py
+++ main.py
@@ -7,4 +7,22 @@ for symb in punctuation:
   text = text.replace(symb, '')
 
 words = text.split()
+
+word_frequency = {}
+
+for word in words:
+  if word in word_frequency:
+    word_frequency[word] += 1
+  else:
+    word_frequency[word] = 1
+
+
 num_words = len(set(words))
+
+print("\nКоличество разных слов:", num_words)
+print("Частота слов:")
+for word, frequency in word_frequency.items():
+    print(f"{word}: {frequency}")
+# 1.перебрать список слов
+# 2. прописать условия для проверки наличия слова в словоре
+# 2. 1 +1 если слово есть, инае присвоить 1
